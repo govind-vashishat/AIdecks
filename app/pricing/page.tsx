9913e573873a41d803033deed561b043812f3173
Govind Vashishat <govindvashishat@gmail.com>
2024-12-08 14:32:48 +0530

    add pricing route

---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pricing = () => {
+  return (
+    <div className='text-center mt-28 text-2xl font-bold'>Pricing section will be released soon...</div>
+  )
+}
+
+export default pricing
